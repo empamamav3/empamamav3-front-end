a17bc0e1bdc68a47cd0538b344c0984a9fbc1328
wzd200 <william.z.dye@gmail.com>
2021-07-10 00:47:22 -0400

    Added dark layer to homepage to dim the brightness of the background

--- empamamav3-frontend/src/components/Home.js
+++ empamamav3-frontend/src/components/Home.js
@@ -2,7 +2,9 @@ export default function Home () {
     return (
         <div>
             <section className='homePage'>
-                <h1>Welcome to Empamama's</h1>
+                <div className='titleContainer'>
+                    <h1>Welcome to Empamama's</h1>
+                </div>
             </section>
         </div>
     )
